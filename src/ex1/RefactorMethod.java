bd10bd4bbb8cdbfe063e3927053ac2a1e6bade3c
Liad Oz <liadozil@gmail.com>
2020-11-02 22:42:42 +0200

    added classes for renaming

--- src/ex1/RefactorMethod.java
+++ src/ex1/RefactorMethod.java
@@ -1,9 +1,15 @@
 package ex1;
 
+import ast.AstNode;
 import ast.Program;
 
 public class RefactorMethod implements RefactorProgram {
     @Override
     public void refactor(Program orig, String originalName, String line, String newName) {
+        LineVisitor linev = new LineVisitor(Integer.getInteger(line)); // TODO: find the scope of the var
+        orig.accept(linev);
+        AstNode scope = linev.getResult();
+        RenameVarVisitor rename = new RenameVarVisitor(originalName, newName); // TODO: rename the var
+        scope.accept(rename);
     }
 }
